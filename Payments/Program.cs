333fe7a38f8c653d951aa2b86b86e059f7fff1ff
claudiojrsx <claudiojrs1993@gmail.com>
2022-03-20 00:47:16 -0300

    Payment | 20/03/2022

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -6,7 +6,24 @@ namespace Payments
     {
         static void Main(string[] args)
         {
+            var paymentTicket = new PaymentsTicket();
+            paymentTicket.Pay();
+            paymentTicket.DueDate = DateTime.Now;
+            paymentTicket.NumberTicket = "123";
+
             Console.WriteLine("Hello World!");
         }
+    }
+
+    class Payments {
+        // Propriedades
+        public DateTime DueDate;
+
+        // Métodos
+        public void Pay() {}
+    }
+
+    class PaymentsTicket : Payments {
+        public string NumberTicket;
     }
 }
